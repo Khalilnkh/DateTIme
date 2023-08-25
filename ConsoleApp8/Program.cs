a703d650a78b36610ebbd903b5cd4a6526cd99f1
Khalilnkh <khalilnkh@code.edu.az>
2023-08-25 13:37:36 +0400

    created

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                var date = DateTime.ParseExact(input, "dd | MM | yyyy", null);
+                Console.WriteLine(date);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            
+
+        }
+    }
+}
---
+++ DateTime2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTime2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(30, 6, 17,0);
+            string input = Console.ReadLine();
+            var date = DateTime.ParseExact(input, "dd/MM/yyyy", null).Add(t1);
+            Console.WriteLine(date);
+            
+            
+        }
+    }
+}
